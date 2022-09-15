b13de62616bccb7ffb15cf7be86972f57f18ec96
Nguyễn Đăng Đức <ducden164@gmail.com>
2022-09-15 10:50:54 +0700

    update

---
+++ assets/js/jquery-main.js
@@ -0,0 +1,6 @@
+//show popup 
+function showPopup() {
+    const openPopup = document.getElementById("popup");
+    openPopup.style.display === 'none' ? openPopup.style.display = "block" : openPopup.style.display = "none";
+
+}
